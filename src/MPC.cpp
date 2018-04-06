95b935bb5070b784d8ed8f3e66bf86f8b6ba3691
jacquestkirk <jacquestkirk@gmail.com>
2018-04-06 02:06:44 -0500

    Update polynomial to 3rd order, fix some update equations

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -114,15 +114,15 @@ class FG_eval {
 			  AD<double> delta0 = vars[delta_start + t - 1];
 			  AD<double> a0 = vars[a_start + t - 1];
 
-			  AD<double> f0 = coeffs[0] + coeffs[1] * x0;
-			  AD<double> psides0 = CppAD::atan(coeffs[1]);
+			  AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2]*x0*x0 + coeffs[3]*x0*x0*x0;
+			  AD<double> psides0 = CppAD::atan(coeffs[1] + 2* coeffs[2] * x0 + 3* coeffs[3]*x0*x0);
 
 
 
 			  // Setup the  the model constraints
 			  fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
 			  fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-			  fg[1 + psi_start + t] = psi1 - (v0 / Lf * delta0 * dt);
+			  fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
 			  fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
 			  fg[1 + cte_start + t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
 			  fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
